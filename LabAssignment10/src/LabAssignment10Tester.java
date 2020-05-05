e524f477381d8378d2ac790d6151dae7bd88a567
Richard511011 <37254288+Richard511011@users.noreply.github.com>
2020-05-04 23:48:14 -0700

    Update LabAssignment10Tester.java

--- LabAssignment10/src/LabAssignment10Tester.java
+++ LabAssignment10/src/LabAssignment10Tester.java
@@ -1,6 +1,6 @@
 
 public class LabAssignment10Tester {
-	//Professor skipped lecture for this lab 4/26/2020
+	
 	public static void main(String[] args) {
 		isBalancedTester();
 		getPredecessorTester();
